d80345107ee0920e3d58e08a4996c5bb1a9f8c3e
Денис Трофимов <d.trofimov@sitt.me>
2020-05-17 18:59:23 +0300

    feat: finished blog comments

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,45 +1,49 @@
+# frozen_string_literal: true
+
 class ArticlesController < ApplicationController
-    include ArticlesHelper
-    def index
-        @articles = Article.all
-    end
+  include ArticlesHelper
+  def index
+    @articles = Article.all
+  end
 
-    def show
-        @article = Article.find(params[:id])
-    end
+  def show
+    @article = Article.find(params[:id])
+    @comment = Comment.new
+    @comment.article_id = @article.id
+  end
 
-    def new
-        @article = Article.new
-    end
+  def new
+    @article = Article.new
+  end
 
-    def create
-        @article = Article.new(article_params)
-        @article.save
+  def create
+    @article = Article.new(article_params)
+    @article.save
 
-        flash.notice = "Article '#{@article.title}' created!"
+    flash.notice = "Article '#{@article.title}' created!"
 
-        redirect_to article_path(@article)
-    end
+    redirect_to article_path(@article)
+  end
 
-    def destroy
-        @article = Article.find(params[:id])
-        @article.destroy
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
 
-        flash.notice = "Article '#{@article.title}' deleted."
+    flash.notice = "Article '#{@article.title}' deleted."
 
-        redirect_to articles_path
-    end
+    redirect_to articles_path
+  end
 
-    def edit
-        @article = Article.find(params[:id])
-    end
+  def edit
+    @article = Article.find(params[:id])
+  end
 
-    def update
-        @article = Article.find(params[:id])
-        @article.update(article_params)
+  def update
+    @article = Article.find(params[:id])
+    @article.update(article_params)
 
-        flash.notice = "Article '#{@article.title}' updated!"
+    flash.notice = "Article '#{@article.title}' updated!"
 
-        redirect_to article_path(@article)
-    end
+    redirect_to article_path(@article)
+  end
 end
